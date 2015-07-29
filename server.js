4d098c966897a00c15daa3f31b76661a5bbb9ca3
Eric Raio <ericraio@gmail.com>
2015-07-28 23:58:45 -0700

    Revert "crap"
    This reverts commit b1ac329e8a82c646d9468c92b4d7fa3207282b72, reversing
    changes made to f905cf23dea7e84021d5b6d7b623124a093b4224.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-#!/usr/bin/env node
-var prerender = require('./lib');
+var prerender = require('prerender');
 
 var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
-});
+	workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+	iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+	phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
+	messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    });
+
 
 
 if (process.env.BASIC_AUTH_USERNAME) {
