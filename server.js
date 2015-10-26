c4233b45efa11ac2d70287198252aff134692188
Eric Raio <ericraio@gmail.com>
2015-10-26 00:14:29 -0700

    adding redis

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var prerender = require('prerender');
 
 var server = prerender({
-	workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-	iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-	phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-	messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
-    });
-
-
+  workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+  iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+  phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
+  messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+});
 
 if (process.env.BASIC_AUTH_USERNAME) {
     server.use(prerender.basicAuth());
@@ -16,6 +14,7 @@ if (process.env.BASIC_AUTH_USERNAME) {
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 server.use(prerender.logger());
+server.use(require('prerender-redis-cache'));
 
 //server.use(prerender.inMemoryHtmlCache());
 //server.use(prerender.blacklist());
